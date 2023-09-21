6206cdc34ebcae55fd17a8bc5e9c0f755b20a998
seoyeon-jung <jungluchia1204@gmail.com>
2023-09-21 22:58:09 +0900

    feat: GlobalStyle 추가 #2
    - background-color 지정
    - text color 지정
    - logo text color 지정

--- src/shared/GlobalStyle.jsx
+++ src/shared/GlobalStyle.jsx
@@ -1,3 +1,19 @@
-const GlobalStyle = () => {};
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+/* textColor가 보통은 하얀색으로 가끔 다른 색상 쓰일 때는 <GlobalStyle textColor="#8B8B8B" /> 형식으로 사용할 수 있습니다=*/
+body {
+    background-color: #16161D;
+    color: ${({ textColor }) => textColor || "#FFFFFF"};
+    font-family: Arial, Helvetica, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+/* logo 텍스트 색상 (추후 수정 가능) */
+.logo {
+    color: #5263FF;
+}
+`;
 
 export default GlobalStyle;
