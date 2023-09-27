f184ed5c32ad4e3d1cd7866cff6240725c2cba7a
seoyeon-jung <jungluchia1204@gmail.com>
2023-09-27 23:50:52 +0900

    design: 비공개스터디방 가입 modal UI 구현 #15

--- src/components/studyroom/StudyItem.jsx
+++ src/components/studyroom/StudyItem.jsx
@@ -4,9 +4,19 @@ import GroupIcon from "../../assets/group.png";
 import LockIcon from "../../assets/lock.png";
 import JoinBtn from "../../assets/Btn_join.png";
 import { useNavigate } from "react-router-dom";
+import useModal from "../../hooks/useModal";
+import { useState } from "react";
 
 export default function StudyItem() {
   const navigate = useNavigate();
+
+  // 모달 custom hook 사용
+  const { isModalOpen, openModal, closeModal, Modal } = useModal();
+
+  // open modal
+  const handleModal = () => {
+    openModal();
+  };
 
   return (
     <Container>
@@ -26,7 +36,19 @@ export default function StudyItem() {
         <Icon src={LockIcon} alt="비공개" />
         비공개
       </div>
-      <img src={JoinBtn} alt="가입하기" />
+      <img src={JoinBtn} alt="가입하기" onClick={handleModal} />
+
+      {/* 비공개 모달로 우선 구현 (추후 공개 modal 추가 예정) */}
+      <Modal style={{ width: "400px", height: "220px" }}>
+        <ModalContent>
+          <div>
+            <ModalBody>
+              이 스터디방은 비공개입니다. 가입코드를 입력해주세요.
+            </ModalBody>
+            <CodeInput type="password" maxLength="4" placeholder="* * * *" />
+          </div>
+        </ModalContent>
+      </Modal>
     </Container>
   );
 }
@@ -56,5 +78,41 @@ const Title = styled.button`
 
   &:hover {
     color: #5263ff;
+  }
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  margin-top: 60px;
+  font-size: 14px;
+  text-align: center;
+`;
+
+const ModalBody = styled.div`
+  margin-bottom: 20px;
+  color: #c7c7c7;
+  text-align: center;
+`;
+
+const CodeInput = styled.input`
+  border: none;
+  outline: none;
+  font-size: 18px;
+  width: 266px;
+  height: 37px;
+  padding: 4px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  background: linear-gradient(to bottom, #636363, #6363634d);
+
+  &::placeholder {
+    color: #acacac;
+    font-size: 18px;
   }
 `;
