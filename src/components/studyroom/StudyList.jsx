87084f5481d98949b213c9f7fe1debdeac0c6ad0
seoyeon-jung <jungluchia1204@gmail.com>
2023-09-27 16:00:17 +0900

    feat: selectBox 컴포넌트 구현 #15
    - 검색창의 selectBox를 구현했습니다
    - study방 List 컴포넌트를 따로 만들었습니다

---
+++ src/components/studyroom/StudyList.jsx
@@ -0,0 +1,4 @@
+/* study방 리스트 컴포넌트 */
+export default function StudyList() {
+  return <div>스터디방 리스트</div>;
+}
--- src/pages/StudyMain.jsx
+++ src/pages/StudyMain.jsx
@@ -1,5 +1,23 @@
+import SelectBox from "../components/studyroom/SelectBox";
+import StudyList from "../components/studyroom/StudyList";
+
 const StudyMain = () => {
-  return <div>스터디방 메인 페이지입니다</div>;
+  return (
+    <>
+      <div>나의 그룹 스터디방 (0)</div>
+      <div>
+        가입된 스터디방이 없네요! 기므바로 스터디방에 가입하고 스터디원과 답변을
+        공유해보세요.
+      </div>
+      <div>
+        <SelectBox />
+        <input placeholder="검색어를 입력하세요." />
+      </div>
+      <div>
+        <StudyList />
+      </div>
+    </>
+  );
 };
 
 export default StudyMain;
