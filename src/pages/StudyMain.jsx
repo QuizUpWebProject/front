6440594894b5bcaae77bdfca3da91a07baa324b5
seoyeon-jung <jungluchia1204@gmail.com>
2023-09-27 16:37:11 +0900

    design: topSection UI 구현 #15
    - 스터디방 만들기 버튼 추가
    - 스터디방 만들기 페이지로 바로가기 Link 추가
    - '나의 그룹 스터디방' UI 구현

--- src/pages/StudyMain.jsx
+++ src/pages/StudyMain.jsx
@@ -1,23 +1,82 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 import SelectBox from "../components/studyroom/SelectBox";
 import StudyList from "../components/studyroom/StudyList";
+import MakeStudy from "../assets/Btn_mstudy.png";
 
 const StudyMain = () => {
   return (
-    <>
-      <div>나의 그룹 스터디방 (0)</div>
-      <div>
-        가입된 스터디방이 없네요! 기므바로 스터디방에 가입하고 스터디원과 답변을
-        공유해보세요.
-      </div>
-      <div>
+    <Wrapper>
+      <TopSection>
+        <Title>나의 그룹 스터디방 (0)</Title>
+        <ImageLink to="/studymake">
+          <Image src={MakeStudy} alt="스터디방 만들기" />
+        </ImageLink>
+      </TopSection>
+
+      <MyStudyList>
+        가입된 스터디방이 없네요! 지금 바로 스터디방에 가입하고 스터디원과
+        답변을 공유해보세요.
+      </MyStudyList>
+
+      <SearchSection>
         <SelectBox />
-        <input placeholder="검색어를 입력하세요." />
-      </div>
+        <SearchInput placeholder="검색어를 입력하세요." />
+      </SearchSection>
+
       <div>
         <StudyList />
       </div>
-    </>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  max-width: 1090px;
+  margin: 0 auto;
+`;
+
+const TopSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
+  padding: 20px 0;
+`;
+
+const Title = styled.div`
+  font-size: 24px;
+  font-weight: bold;
+  width: 50%;
+  text-align: left;
+`;
+
+const ImageLink = styled(Link)`
+  text-align: right;
+  text-decoration: none;
+`;
+
+const Image = styled.img`
+  width: auto;
+  height: 100%;
+  text-align: right;
+`;
+
+const MyStudyList = styled.div`
+  padding: 20px 0;
+  border-bottom: 1px solid #ccc;
+`;
+
+const SearchSection = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px 0;
+  justify-content: flex-end;
+  margin-top: 50px;
+`;
+
+const SearchInput = styled.input`
+  magin-left: 10px;
+`;
 
 export default StudyMain;
