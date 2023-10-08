7f0ed5f8e7b4a223d76710a2b5a0dd63d26ff74c
seoyeon-jung <jungluchia1204@gmail.com>
2023-10-08 16:45:52 +0900

    design: 스터디방 만들기 틀 잡기 #19

--- src/pages/StudyMake.jsx
+++ src/pages/StudyMake.jsx
@@ -1,5 +1,47 @@
+import styled from "styled-components";
+
 const StudyMake = () => {
-  return <div>스터디방 만들기 페이지입니다</div>;
+  return (
+    <Wrapper>
+      <div>
+        <div>그룹명*</div>
+        <input />
+        <button>중복 확인</button>
+      </div>
+
+      <div>
+        <div>그룹 카테고리*</div>
+        {/* select (front/back) */}
+      </div>
+
+      <div>
+        <div>제한 인원수*</div>
+        {/* select (1~50) */}
+      </div>
+
+      <div>
+        <div>
+          참여코드 (선택사항) 공개 스터디방으로 원할 경우 빈칸으로 제출하세요.
+        </div>
+        <input
+          text="password"
+          placeholder="참여코드 설정은 숫자 4자리 입니다."
+        />
+      </div>
+
+      <div>
+        <div>소개글</div>
+        <textarea placeholder="궁금할 스터디원들에게 어떤 스터디방인지 설명해주세요. ex) 규칙사항, 준비하는 문제에 대해 간략하게 적어보세요." />
+      </div>
+
+      <button>방 만들기</button>
+    </Wrapper>
+  );
 };
 
 export default StudyMake;
+
+const Wrapper = styled.div`
+  max-width: 1090px;
+  margin: 0 auto;
+`;
