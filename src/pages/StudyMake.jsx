a8b57f17631950f3cebd93980e816ea6d09a099c
seoyeon-jung <jungluchia1204@gmail.com>
2023-10-08 17:28:22 +0900

    design: input, textarea style 추가 #19

--- src/pages/StudyMake.jsx
+++ src/pages/StudyMake.jsx
@@ -7,8 +7,11 @@ const StudyMake = () => {
     <Wrapper>
       <div>
         <div>그룹명*</div>
-        <input />
-        <button>중복 확인</button>
+        <div>
+          <GroupNameInput />
+          <ConfirmBtn>중복 확인</ConfirmBtn>
+        </div>
+        <div>사용 가능한 특수문자는 (_/-/@/.)입니다.</div>
       </div>
 
       <div>
@@ -27,7 +30,7 @@ const StudyMake = () => {
         <div>
           참여코드 (선택사항) 공개 스터디방으로 원할 경우 빈칸으로 제출하세요.
         </div>
-        <input
+        <CodeInput
           text="password"
           placeholder="참여코드 설정은 숫자 4자리 입니다."
         />
@@ -35,7 +38,7 @@ const StudyMake = () => {
 
       <div>
         <div>소개글</div>
-        <textarea placeholder="궁금할 스터디원들에게 어떤 스터디방인지 설명해주세요. ex) 규칙사항, 준비하는 문제에 대해 간략하게 적어보세요." />
+        <IntroduceArea placeholder="궁금할 스터디원들에게 어떤 스터디방인지 설명해주세요. ex) 규칙사항, 준비하는 문제에 대해 간략하게 적어보세요." />
       </div>
 
       <MakeBtn>방 만들기</MakeBtn>
@@ -50,6 +53,63 @@ const Wrapper = styled.div`
   margin: 0 auto;
 `;
 
+const GroupNameInput = styled.input`
+  width: 642px;
+  height: 42px;
+  padding: 5px;
+  background-color: #3f424e;
+  border: none;
+  border-radius: 6px;
+  margin-right: 20px;
+  color: #ffffff;
+`;
+
+const ConfirmBtn = styled.button`
+  width: 180px;
+  height: 42px;
+  border: none;
+  border-radius: 5px;
+  background-color: #5263ff;
+  color: #ffffff;
+  font-size: 14px;
+  font-weight: 600;
+  margin-left: 20px;
+`;
+
+const CodeInput = styled.input`
+  width: 866px;
+  height: 42px;
+  padding: 5px;
+  background-color: #3f424e;
+  border: none;
+  border-radius: 6px;
+  color: #ffffff;
+
+  &::placeholder {
+    color: #ffffff;
+    padding: 5px;
+    font-size: 14px;
+    font-weight: 400;
+  }
+`;
+
+const IntroduceArea = styled.textarea`
+  width: 866px;
+  height: 93px;
+  padding: 5px;
+  background-color: #3f424e;
+  border: none;
+  border-radius: 6px;
+  color: #ffffff;
+
+  &::placeholder {
+    color: #ffffff;
+    padding: 5px;
+    font-size: 14px;
+    font-weight: 400;
+  }
+`;
+
 const MakeBtn = styled.button`
   width: 866px;
   height: 39px;
@@ -57,4 +117,6 @@ const MakeBtn = styled.button`
   border-radius: 6px;
   background-color: #5263ff;
   color: #ffffff;
+  font-size: 14px;
+  font-weight: 500;
 `;
