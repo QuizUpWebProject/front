ed3169e5c5917768fba056f0067de11e49ffe94b
seoyeon-jung <jungluchia1204@gmail.com>
2023-10-12 16:38:48 +0900

    design: error message 추가 #6
    - 아이디, 비밀번호가 다를 시 나오는 error message를 추가했습니다

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -4,10 +4,17 @@ import { useState } from "react";
 
 export default function LoginForm() {
   const [isChecked, setIsChecked] = useState(false); // 로그인 유지하기 버튼 클릭 상태관리
+  const [isError, setIsError] = useState(false); // 에러 상태 관리
 
   // 로그인 유지하기 기능 구현 필요
   const handleCheckClick = () => {
     setIsChecked(!isChecked);
+  };
+
+  // 로그인 버튼 클릭 시 기능 구현 예정
+  const handleLoginClick = () => {
+    // 아이디, 비밀번호 일치하지 않을 떄 error message 구현
+    setIsError(true);
   };
 
   return (
@@ -36,7 +43,14 @@ export default function LoginForm() {
         </CheckLabel>
       </CheckContainer>
 
-      <LoginBtn>로그인</LoginBtn>
+      {isError && (
+        <ErrorMessage>
+          아이디(이메일) 또는 비밀번호를 잘못 입력했습니다. 입력하신 내용을 다시
+          확인해주세요.
+        </ErrorMessage>
+      )}
+
+      <LoginBtn onClick={handleLoginClick}>로그인</LoginBtn>
     </div>
   );
 }
@@ -89,11 +103,18 @@ const LoginBtn = styled.button`
   align-items: center;
 `;
 
+const ErrorMessage = styled.div`
+  color: #ff0000;
+  font-size: 15px;
+  font-weight: 400;
+  margin-bottom: 20px;
+`;
+
 const CheckContainer = styled.button`
   font-size: 15px;
   font-weight: 400;
   margin-top: 30px;
-  margin-bottom: 30px;
+  margin-bottom: 20px;
   background-color: transparent;
   border: none;
   color: ${(props) => (props.isChecked ? "#5263ff" : "inherit")};
