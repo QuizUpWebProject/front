068c9dc292baf1dace03c4b735c4e8aee1e3999d
seoyeon-jung <jungluchia1204@gmail.com>
2023-10-12 15:39:27 +0900

    design: loginform 만들기 #6

---
+++ src/components/login/LoginForm.jsx
@@ -0,0 +1,17 @@
+export default function LoginForm() {
+  return (
+    <div>
+      <div>
+        <div>아이디</div>
+        <input />
+      </div>
+
+      <div>
+        <div>비밀번호</div>
+        <input />
+      </div>
+
+      <button>로그인</button>
+    </div>
+  );
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,24 @@
+import styled from "styled-components";
+import LoginForm from "../components/login/LoginForm";
+
 const Login = () => {
-    return (
-        <div>
-            Login 페이지입니다
-        </div>
-    )
-}
+  return (
+    <Wrapper>
+      <Title>로그인</Title>
+      <LoginForm />
+    </Wrapper>
+  );
+};
 
 export default Login;
+
+const Wrapper = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+`;
+
+const Title = styled.div`
+  color: #5263ff;
+  font-size: 40px;
+  font-weight: 600;
+`;
