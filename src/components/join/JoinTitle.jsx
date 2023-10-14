64b36d2e52c8f2304f6de49fcd4348f40aac580a
seoyeon-jung <jungluchia1204@gmail.com>
2023-10-14 21:42:58 +0900

    design: 회원가입 Title UI 구현 #7

--- src/components/join/JoinTitle.jsx
+++ src/components/join/JoinTitle.jsx
@@ -2,11 +2,37 @@ import styled from "styled-components";
 
 export default function JoinTitle() {
   return (
-    <div>
-      <div>회원가입</div>
-      <div>① 약관동의</div>
-      <div>② 정보입력</div>
-      <div>③ 가입 완료</div>
-    </div>
+    <TitleContainer>
+      <Join>회원가입</Join>
+      <SectionBox>
+        <JoinSection>① 약관동의</JoinSection>
+        <JoinSection>② 정보입력</JoinSection>
+        <JoinSection>③ 가입 완료</JoinSection>
+      </SectionBox>
+    </TitleContainer>
   );
 }
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
+
+const Join = styled.div`
+  font-size: 40px;
+  font-weight: 600;
+  color: #5263ff;
+  margin-right: 40px;
+`;
+
+const SectionBox = styled.div`
+  margin-left: 20px;
+  display: flex;
+`;
+
+const JoinSection = styled.div`
+  font-size: 15px;
+  font-weight: 500;
+  color: #7e7e7e;
+  margin-right: 40px;
+`;
