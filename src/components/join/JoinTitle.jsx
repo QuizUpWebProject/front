724d0d90dfa14ea4ad95d7f6613e0dcfadcece88
seoyeon-jung <jungluchia1204@gmail.com>
2023-10-14 21:35:51 +0900

    design: 회원가입 공통 Title 구현 #7
    - 회원가입과 순서는 공통이므로 컴포넌트로 구현했습니다

---
+++ src/components/join/JoinTitle.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default function JoinTitle() {
+  return (
+    <div>
+      <div>회원가입</div>
+      <div>① 약관동의</div>
+      <div>② 정보입력</div>
+      <div>③ 가입 완료</div>
+    </div>
+  );
+}
