96b755a11d9475dd1602763470739fcd09eba1f6
seoyeon-jung <jungluchia1204@gmail.com>
2023-10-14 22:24:59 +0900

    design: 회원가입 약관동의 페이지 UI 구현 #7
    - 컴포넌트로 따로 빼서 구현했습니다

--- src/pages/JoinAgree.jsx
+++ src/pages/JoinAgree.jsx
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 import JoinTitle from "../components/join/JoinTitle";
+import JoinAgreeBox from "../components/join/JoinAgreeBox";
 
 const JoinAgree = () => {
   return (
     <Wrapper>
       <JoinTitle section1="#5263ff" />
+
+      <JoinAgreeBox />
     </Wrapper>
   );
 };
