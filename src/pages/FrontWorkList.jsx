59f392c7b21743d35713c3e74c86ba44e610ace6
Jisu0528 <71203375+Jisu0528@users.noreply.github.com>
2023-10-18 19:36:57 +0900

    design: Front 문제집 리스트 페이지 TopSection UI 구현 #20 - 문제집 제목과 작성자명 표시 - 전체 학습하기 버튼 - 추천하기 버튼

--- src/pages/FrontWorkList.jsx
+++ src/pages/FrontWorkList.jsx
@@ -1,5 +1,107 @@
+import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import { useState } from "react";
+import LikeIcon from "../assets/heart.png";
+import CmtIcon from "../assets/chat.png";
+import UserIcon from "../assets/people.png";
+import LikeBtn from "../assets/Btn_like.png";
+import AllBtn from "../assets/Btn_all.png";
+
 const FrontWorkList = () => {
-  return <div>프론트엔드 스터디방 메인 페이지입니다</div>;
+    const [textValue, setTextValue] = useState(0);    // 추천수 올리기
+
+    const increaseLike = () => {
+      setTextValue(textValue + 1);
+    };
+  
+  return (
+    <Wrapper>
+      <TopSection>
+        <TopTitle>
+          정보처리기사 실기_2023 <Works>총 91문제</Works>
+        </TopTitle>
+
+        <TopContent>
+          <WorkbookInfo>
+            <IconTextWrapper>
+              <Icon src={UserIcon} alt="작성자"></Icon>소밍밍
+            </IconTextWrapper>
+            <LikeCmt>
+              <IconTextWrapper>
+                <Icon src={LikeIcon} alt="추천"></Icon>{textValue}
+              </IconTextWrapper>
+              <IconTextWrapper>
+                <Icon src={CmtIcon} alt="댓글"></Icon>04
+              </IconTextWrapper>
+            </LikeCmt>
+          </WorkbookInfo>
+          <Buttons>
+            <Link to='/front/:worklistid/:workid'>
+              <img src={AllBtn} alt="전체학습하기" />
+            </Link>
+            <img src={LikeBtn} alt="추천하기" onClick={increaseLike} style={{cursor: 'pointer'}}/>
+          </Buttons>
+        </TopContent>
+      </TopSection>
+
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  max-width: 1090px;
+  margin: 0 auto;
+`;
+
+const TopSection = styled.div`
+  padding: 20px 0;
+  margin-bottom: 50px;
+`;
+
+const TopTitle = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  margin-bottom: 17px;
+`;
+
+const Works = styled.div`
+  font-size: 15px;
+  margin-left: 42px;
+`;
+
+const TopContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 16px;
+`;
+
+const WorkbookInfo = styled.div`
+ display: flex;
+ gap: 52px; 
+`;
+
+const LikeCmt = styled.div`
+  gap: 17px;
+  display: flex;
+`;
+
+const IconTextWrapper = styled.div`
+  display: flex;
+  align-items: center; 
+`;
+
+const Icon = styled.img`
+  margin-right: 5px; 
+`;  
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 17px;
+  text-align: right;
+  align-items: center;
+`;
 
 export default FrontWorkList;
