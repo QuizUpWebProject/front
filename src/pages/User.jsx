6ffd6d5244ed8c6c2eeb8eec1a8becc942ef0fea
Jisu0528 <71203375+Jisu0528@users.noreply.github.com>
2023-11-15 02:05:13 +0900

    design: 유저페이지 UI 구현 (#5) - 나의 문제집 슬라이드 추가 - 학습중인 문제집 추가 - footer 추가 - dropdown 컴포넌트 구현

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,5 +1,69 @@
+import styled from "styled-components";
+import MakeWork from "../assets/Btn_mwork.png";
+import Dropdown from "../components/user/StudyDropdown";
+import MyWorkSlide from "../components/user/MyWorkSlide";
+import StudyWorkSlide from '../components/user/StudyWorkSlide';
+import Footer from '../shared/Footer';
+
 const User = () => {
-  return <div>유저의 페이지입니다</div>;
+  return (
+    <Wrapper>
+      <Line1 />
+      <TitleSection>
+        <Title>나의 문제집 [4]</Title>
+        <Icon src={MakeWork} alt="문제집 만들기"/>
+      </TitleSection>
+      <MyWorkSlide />
+
+      <Line2 />
+      <TitleSection>
+        <Title>학습중인 문제집 [4]</Title>
+        <Dropdown />
+      </TitleSection>
+      <StudyWorkSlide />
+      <Footer />
+      
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  max-width: 1090px;
+  margin: 0 auto;
+`;
+
+const Line1 = styled.hr`
+  width: 1090px;
+  border: 1px solid #868686;
+  margin-top: 50px;
+  margin-bottom: 45px;
+`;
+
+const Line2 = styled.hr`
+  width: 1090px;
+  border: 1px solid #454545;
+  margin-top: 64px;
+  margin-bottom: 45px;
+`;
+
+const TitleSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 0;
+`;
+
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  width: 50%;
+  text-align: left;
+`;
+
+const Icon = styled.img`
+  margin-left: 14px;
+  font-size: 13px;
+  cursor: pointer;
+`;
 
 export default User;
