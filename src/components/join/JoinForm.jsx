444032cc27bb4979dc1a649f1e61b809a848530b
seoyeon-jung <jungluchia1204@gmail.com>
2023-12-19 17:32:59 +0900

    refactor: 비밀번호 input 컴포넌트화 #40

--- src/components/join/JoinForm.jsx
+++ src/components/join/JoinForm.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import EmailCheck from "./EmailCheck";
 import NicknameCheck from "./NicknameCheck";
 import useModal from "../../hooks/useModal";
+import PasswordForm from "./PasswordForm";
 
 export default function JoinForm() {
   const navigate = useNavigate();
@@ -46,34 +47,15 @@ export default function JoinForm() {
     <Wrapper>
       <EmailCheck email={email} setEmail={setEmail} />
 
-      <Container>
-        <Label>
-          비밀번호<span>*</span>
-        </Label>
-        <Input
-          type="password"
-          placeholder="비밀번호를 입력해주세요. "
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Text
-          style={{
-            color: isValid ? "#939393" : "#ff0000",
-          }}
-        >
-          영문,숫자,특수문자를 포함하여 8-13자로 입력해주세요.
-        </Text>
-        <Input
-          type="password"
-          placeholder="비밀번호를 다시 한번 더 입력해주세요."
-          value={passwordConfirm}
-          onChange={(e) => setpasswordConfirm(e.target.value)}
-          onBlur={handleCheckPassword}
-        />
-        {isError && (
-          <PasswordMisMatch>비밀번호가 일치하지 않습니다.</PasswordMisMatch>
-        )}
-      </Container>
+      <PasswordForm
+        password={password}
+        passwordConfirm={passwordConfirm}
+        onPasswordChange={setPassword}
+        onPasswordConfirmChange={setpasswordConfirm}
+        onBlur={handleCheckPassword}
+        isValid={isValid}
+        isError={isError}
+      />
 
       <NicknameCheck nickname={nickname} setNickname={setNickname} />
 
@@ -94,54 +76,6 @@ export default function JoinForm() {
 const Wrapper = styled.div`
   margin-top: 60px;
   margin-bottom: 40px;
-`;
-
-const Container = styled.div`
-  margin-bottom: 30px;
-`;
-
-const Label = styled.div`
-  font-size: 16px;
-  font-weight: 600;
-  line-height: 19.5px;
-  margin-bottom: 14px;
-
-  span {
-    color: #ff0000;
-  }
-`;
-
-const Input = styled.input`
-  width: 866px;
-  height: 42px;
-  background-color: #3f424e;
-  border: none;
-  border-radius: 6px;
-  color: #ffffff;
-  padding: 5px;
-  margin-bottom: 15px;
-
-  &::placeholder {
-    color: #ffffff;
-    font-size: 14px;
-    font-weight: 400;
-    padding: 5px;
-  }
-`;
-
-const Text = styled.div`
-  color: #939393;
-  font-size: 13px;
-  font-weight: 400;
-  line-height: 29px;
-  margin-bottom: 15px;
-`;
-
-const PasswordMisMatch = styled.div`
-  color: #ff0000;
-  font-weight: 600;
-  font-size: 14px;
-  margin-top: 10px;
 `;
 
 const Button = styled.button`
